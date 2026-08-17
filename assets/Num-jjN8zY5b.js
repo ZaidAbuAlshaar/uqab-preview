945a1f2961b77767ad4ee35bf2eb2fb09386c16c
Zaid Abu Alshaar <abualshaarzaid@gmail.com>
2026-08-17 15:10:33 +0300

    preview: 2026-08-17_1510

--- assets/Num-jjN8zY5b.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-f8QyIucC.js";function e({children:n,className:t}){return r.jsx("bdi",{className:["num",t].filter(Boolean).join(" "),children:n})}function i(n,t=""){return`${n<0?"−":n>0?"+":""}${Math.abs(n)}${t}`}function a(n){return i(n,"%")}function f(n){return`⁦${n}⁩`}export{e as N,a as f,f as i};
